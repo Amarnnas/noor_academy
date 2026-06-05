cb4681798ceebaef6dfb21855613691bbc857ca7
mhmdalfayz818-gif <mhmdalfayz818@gmail.com>
2026-06-05 12:31:12 +0300

    Modify constants for contact and social links
    Updated contact information and social media links.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,9 +3,9 @@ export const SITE = {
   tagline: "طوّر مهاراتك اللغوية مع أفضل المدربين المعتمدين",
   description: "أكاديمية نور للغات – وجهتك الأولى لتعلم اللغات عبر الإنترنت مع مدربين معتمدين ومناهج حديثة",
   email: "info@nooracademy.com",
-  phone: "+966 55 123 4567",
-  whatsapp: "+966551234567",
-  address: "الرياض، المملكة العربية السعودية",
+  phone: "+256791359856",
+  whatsapp: "+256767033631",
+  address: "كمبالا ، هام تاور - جوار جامعة ماكرري ",
 };
 
 export const NAV_LINKS = [
@@ -17,9 +17,9 @@ export const NAV_LINKS = [
 ];
 
 export const SOCIAL_LINKS = {
-  facebook: "https://facebook.com/nooracademy",
-  twitter: "https://twitter.com/nooracademy",
+  facebook: "https://www.facebook.com/profile.php?id=61555636659072",
+  twitter: "https://www.tiktok.com/@dr_noor_aldeen?_r=1&_t=ZS-96x3MeLrh7E",
   instagram: "https://instagram.com/nooracademy",
   linkedin: "https://linkedin.com/company/nooracademy",
-  youtube: "https://youtube.com/@nooracademy",
+  youtube: "https://youtube.com/@nooraldeen123-d2n?si=WADbWU8WkirMXxL0",
 };
